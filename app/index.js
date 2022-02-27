398dae1e9a6b6393034c976c8cb4b37d5488a3f9
John Watson <johnwatson484@hotmail.com>
2022-02-27 12:46:39 +0000

    Add logic

--- app/index.js
+++ app/index.js
@@ -1,13 +1,19 @@
+const storage = require('./storage')
+
 module.exports = async function (context, req) {
   context.log('JavaScript HTTP trigger function processed a request.')
-
   const sourceFile = (req.query.sourceFile || (req.body && req.body.sourceFile))
-  const responseMessage = sourceFile
-    ? `Successfully transferred ${sourceFile}`
-    : 'No file provided'
+
+  if (!sourceFile) {
+    context.res = {
+      body: 'Source file not provided, no action taken'
+    }
+  }
+
+  storage.connect(process.env.BATCH_STORAGE)
+  storage.transferFile(sourceFile)
 
   context.res = {
-    // status: 200, /* Defaults to 200 */
-    body: responseMessage
+    body: `Successfully transferred ${sourceFile}`
   }
 }
